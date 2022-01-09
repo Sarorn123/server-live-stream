c4721a90d05eca4e153a572feea64a8e36a46a2e
rorn <rysarorn8@gmail.com>
2022-01-09 17:55:49 +0700

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("sent_message", (back_message) => {
+    socket.broadcast.emit("message", back_message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("1 user dis");
+  });
+});
+
+server.listen(4001, () => {
+  console.log("SERVER IS RUNING");
+});
